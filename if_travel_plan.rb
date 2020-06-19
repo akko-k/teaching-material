ab5adecdbddc91293e459f8ee688fec82a2abec2
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-06-19 16:29:55 +0900

    fix-3 program of if_travel_plan.rb

--- if_travel_plan.rb
+++ if_travel_plan.rb
@@ -13,25 +13,27 @@ puts "#{i}. #{plan[:destination]}(¥#{plan[:price].to_s(:delimited)})"
 end
 
 while true
-print "プランを選択 >"
+  print "プランを選択 >"
 
-selected_number = gets.chomp.to_i
+  selected_number = gets.chomp.to_i
 
-if   (1..3).include?(selected_number)
-  selected_plan = plans[selected_number -1][:destination]
-  puts "#{selected_plan}ですね、何人で行きますか？"
-  break
-else
+  break if(1..3).include?(selected_number)
   puts "不正な値です。1〜３の番号を入力して下さい"
   next
 end
 
-end
+selected_plan = plans[selected_number -1][:destination]
+puts "#{selected_plan}ですね、何人で行きますか？"
 
 while true
-print "人数を入力 >"
+  print "人数を入力 >"
 
-number_of_people=gets.chomp.to_i
+  number_of_people=gets.chomp.to_i
+
+  break if number_of_people >= 1
+  puts "不正な値です。1以上を入力して下さい"
+  next
+end
 
 selected_plan_price = plans[selected_number -1][:price]
 
@@ -41,13 +43,6 @@ discounted_total_price = number_of_people * selected_plan_price * 0.9
 if number_of_people >= 5
   puts "#{number_of_people}人以上なので10%割引となります"
   puts "合計料金：¥#{discounted_total_price.round.to_s(:delimited)}"
-  break
-elsif number_of_people >= 1
-  puts "合計料金：¥#{total_price.to_s(:delimited)}"
-  break
 else
-  puts "不正な値です。1以上を入力して下さい"
-  next
-end
-
+  puts "合計料金：¥#{total_price.round.to_s(:delimited)}"
 end
