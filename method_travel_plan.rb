19708c356c41ab2370bfba7004bc94b58d2a57ac
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-06-26 00:29:26 +0900

    fix-2 program of method_travel_plan.rb

--- method_travel_plan.rb
+++ method_travel_plan.rb
@@ -6,6 +6,41 @@ def disp_plan(plans)
   end
 end
 
+def select_plan(plans)
+  puts "旅行プランを選択してください"
+  print "プランを選択 >"
+  while true
+    selected_number = gets.chomp.to_i
+    break if(1..3).include?(selected_number)
+    puts "不正な値です。1〜３の番号を入力して下さい"
+  next
+  end
+  return plans[selected_number -1]
+end
+
+def select_number_of_people(selected_plan)
+  puts "#{selected_plan[:destination]}ですね、何人で行きますか？"
+  print "人数を入力 >"
+  while true 
+    number_of_people=gets.chomp.to_i
+    break if number_of_people >= 1
+    puts "不正な値です。1以上を入力して下さい"
+  next
+  end
+  return number_of_people
+end
+
+def info_total_price(selected_plan, number_of_people)
+  total_price = number_of_people * selected_plan[:price]
+  discounted_total_price = (total_price * 0.9).round
+  if number_of_people >= 5
+   puts "5人以上なので10%割引となります"
+   puts "合計料金：¥#{discounted_total_price.to_s(:delimited)}"
+  else
+  puts "合計料金：¥#{total_price.to_s(:delimited)}"
+  end
+end
+
 plans = [
   { destination: "沖縄旅行", price: 10000 },
   { destination: "北海道旅行", price: 20000 },
@@ -13,29 +48,6 @@ plans = [
 ]
 
 disp_plan(plans)
-
-print "プランを選択 >"
-
-def select_plan(plans)
-  selected_number = gets.chomp.to_i
-  if selected_number < 1 ||  3 < selected_number
-    return "不正な値です。1〜３の番号を入力して下さい"
-  end
-  return plans[selected_number -1]
-end
-
 selected_plan = select_plan(plans)
-
-puts "#{selected_plan[:destination]}ですね、何人で行きますか？"
-print "人数を入力 >"
-
-def select_number_of_people(selected_plan)
-  # コード
-end
-
-def info_total_price(selected_plan, number_of_people)
-  # コード
-end
-
 number_of_people = select_number_of_people(selected_plan)
 info_total_price(selected_plan, number_of_people)
